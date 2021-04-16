d6ffb7cb655df7d06d65fd7627174816a5bd9e02
chenzhizs <chenzhi@qq.com>
2021-04-16 11:41:32 +0900

    第一次完成了，并且自己写上说明。 补充说明

--- app/src/main/java/com/example/rxdemo1/MainActivity.java
+++ app/src/main/java/com/example/rxdemo1/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.rxdemo1;
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private String greeting = "greeting hello RxJava";
+    private String greeting2 = "greeting hello RxJava2";
     private Observable<String> myObservable;
     private Observer<String> myObserver;
 
@@ -26,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.tvGreeting);
 
-        //这里就说明了，myObservable是指向greeting这个变量，和它一直连接着。就是说Observable (可观察者，即被观察者)指向哪个变量
-        myObservable = Observable.just(greeting);
+        //Observable部分,被观察者部分。就是说Observable (可观察者，即被观察者)指向哪个变量
+        myObservable = Observable.just(greeting, greeting2) // 在onNext时，第一个运行的是greeting, 第二个运行是greeting2
+                .subscribeOn(Schedulers.io())               // 指定 subscribe() 发生在 IO 线程
+                .observeOn(AndroidSchedulers.mainThread()); // 指定 Subscriber 的回调发生在主线程
 
-        //写了这句后，就会自动的出onSubscribe，onNext，onError，onComplete的函数
+        //Subscriber部分，观察者部分。写了这句后，就会自动的出onSubscribe，onNext，onError，onComplete的函数
         myObserver = new Observer<String>() {
 
             //刚运行时，第一个就是调用这个函数
@@ -41,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             //第二个就是调用这个函数
             @Override
             public void onNext(@NonNull String s) {
-                Log.e(TAG, "onNext: ");
+                Log.e(TAG, "onNext: s:" + s);
                 textView.setText(s);
 
             }
